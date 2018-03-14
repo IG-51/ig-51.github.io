3dd036ccffa74a66bd42a36b8edb0d1b599ab31b
IG-51 <ivangorbach@gmail.com>
2018-03-14 16:40:43 +0300

    Файлы WebDesignGoPro

---
+++ WebDesignGoPro/js/script.js
@@ -0,0 +1,41 @@
+// добавление классов active
+$(document).ready(function(){
+	$( '.header__menu a' ).on( 'click', function () {
+		$( '.header__menu' ).find( 'li.active' ).removeClass( 'active' );
+		$( this ).parent( 'li' ).addClass( 'active' );
+	});
+
+	$( '.banner__rhombs a' ).on( 'click', function () {
+		$( '.banner__rhombs' ).find( '.active' ).removeClass( 'active' );
+		$( this ).children( '' ).addClass( 'active' );
+	});
+
+	$( '.team__member' ).on( 'click', function () {
+		$( '.team' ).find( '.team__member.active' ).removeClass( 'active' );
+		$( this ).addClass( 'active' );
+	});
+
+	$( '.slider__rhombs a' ).on( 'click', function () {
+		$( '.slider__rhombs' ).find( '.active' ).removeClass( 'active' );
+		$( this ).children( '' ).addClass( 'active' );
+	});
+});
+
+// добавление класса is-active
+$(document).ready(function(){
+	$( '.hamburger' ).click(function () {
+		$( this ).toggleClass('is-active');
+		$( '.header__menu ul' ).fadeToggle();
+	});
+});
+
+// иницилизация wow.js
+wow = new WOW(
+{
+	boxClass:     'wow',						// default
+	animatedmateClass: 'animated',	// default
+	offset:       0,								// default
+	mobile:       true,							// default      
+	live:         true							// default
+})
+wow.init();
