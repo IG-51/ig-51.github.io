0e100bfd4c012e4fc3615b0a825035310b1636f9
IG-51 <ivangorbach@gmail.com>
2018-06-06 17:20:30 +0300

    Add files via upload

--- LandingZap/js/scripts.js
+++ LandingZap/js/scripts.js
@@ -13,4 +13,26 @@ $(document).ready(function(){
       }
     ]
   });
+
+
+  // модальное
+  $('a.go').click( function(event){
+		event.preventDefault();
+		$('#overlay').fadeIn(400,
+		 	function(){
+				$('#modalForm') 
+					.css('display', 'block')
+					.animate({opacity: 1, top: '50%'}, 200);
+		});
+  });
+  
+	$('#modalClose, #overlay').click( function(){
+		$('#modalForm')
+			.animate({opacity: 0, top: '45%'}, 200,
+				function(){
+					$(this).css('display', 'none');
+					$('#overlay').fadeOut(400);
+				}
+			);
+	});
 });
